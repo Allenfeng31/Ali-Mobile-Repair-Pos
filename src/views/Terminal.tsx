053e231fa468205d177a76351c7223258c89bbd4
Store Owner <store@example.com>
2026-04-10 15:16:19 +1000

    Update Terminal.tsx

--- src/views/Terminal.tsx
+++ src/views/Terminal.tsx
@@ -19,6 +19,7 @@ import {
   UserCircle,
   ChevronDown
 } from 'lucide-react';
+import { cn } from '@/lib/utils';
 import { CustomerReservation, Customer, InventoryItem, Order, OrderItem } from '../types';
 import { motion, AnimatePresence } from 'motion/react';
 import { InvoiceModal } from '../components/InvoiceModal';
